096bb1479da36f1499cdf3ac90a1d36b9c1af37c
trevel <112418455+trevelyen@users.noreply.github.com>
2025-08-20 00:19:25 +0100

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -5,9 +5,7 @@ export default function TealstreetPage() {
   return (
     <div className='p-4 min-h-screen bg-neutral-900'>
       <h1 className='text-2xl font-bold text-white'>Tealstreet Iterator</h1>
-      <div className='mt-20 max-w-3xl mx-auto bg-neutral-700 border border-neutral-500'>
         <TealstreetComponent />
-      </div>
     </div>
   )
 }
