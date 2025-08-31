6e31dc6b3b274391298d7ad0fed45ca6ef48861e
trevelyen <thetrevelyen@gmail.com>
2025-08-31 12:27:03 +0100

    Trivial

--- src/tealstreet-iterator/_BUILD-HERE/component-iterate.tsx
+++ src/tealstreet-iterator/_BUILD-HERE/component-iterate.tsx
@@ -1,7 +1,7 @@
 const CustomModuleName = 'balanceDisplay'
 // Choose a CustomModuleName for this script to automatically version
 // Make sure to change the name to something else when starting a brand new custom module
-// After confirming the desired name, save the script, then start iteration with pnpm run dev
+// After confirming the desired name, save the script, then start iteration with pnpm dev
 // Each time you save this script, a new version of the custom module will be created in your saved modules
 
 
